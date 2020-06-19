90ae7476333fae07eb6b4618c201a39dfe12b93b
Martijn <Huiskesmartijn@live.nl>
2020-06-19 22:24:00 +0200

    Comments added for readibility

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@ const float maximumTemperatureSlope = 8.3;
 const int minute = 60000; // 1 minute = 60000ms
 const int offSeasonIterations = 4;
 const int offSeasonSleepDuration = 60 * minute;
-//const int timeUntilReceive = 3000;
 const int groupNumber = 1;
 const int syncThresholdNormal = 20;
 const int syncThresholdOffSeason = 5;
@@ -39,53 +38,52 @@ const float maxTempSlope = (10.0 * maximumTemperatureSlope) / 60; //10th degrees
 
 HardwareSerial UARTInterface(PB_7, PB_6);
 
-int checkEnergy()
+int checkEnergy() // Reads voltage and returns number of transmissions that can be done with available energy
 {
-  analogWrite(PA_1, HIGH);
-  delay(4);
+  analogWrite(PA_1, HIGH);             // Turn on energy monitoring circuit
+  delay(4);                            // wait for settling time
   float capVoltage = analogRead(PA_2); //read voltage from io pin
   capVoltage = map(capVoltage, 0, 4095, 0, 1.7);
-  //Calculate energy from capvoltage
   float availableEnergy = 0.5 * 15 * sq(capVoltage);
   float currentConsumption = (24.5 + 85 * (capVoltage * 5.5 / 1.7)) * pow(10, -6);
-  float energyLeft = availableEnergy - currentConsumption * 12;
+  float energyLeft = availableEnergy - currentConsumption * 12; // calculate energy left after 12 hours of operation in current mode
   float energyPerTransmission = 0.04968;
-  float numberOfTransmissions = energyLeft / energyPerTransmission;
+  float numberOfTransmissions = energyLeft / energyPerTransmission; //calculate number of transmissions left
   return (int)numberOfTransmissions;
 }
 
 bool receive()
 {
-  if (UARTInterface.readString() == "ok")
+  if (UARTInterface.readString() == "ok") //UART transmission succesfull
   {
-    LowPower.deepSleep(5000); //sleep for a maximum of 5 seconds to ensure wakup after transmission
+    LowPower.deepSleep(5000); //sleep for a maximum of 5 seconds to ensure wakeup after transmission
     String receivedMessage = UARTInterface.readString();
     UARTInterface.end();
     if (receivedMessage.startsWith("mac_rx")) //mac_rx <portno> <data>
-    {                                         // adjust for different port numbers
-      String receivedData = receivedMessage.substring(receivedMessage.lastIndexOf(" ") + 1);
+    {
+      String receivedData = receivedMessage.substring(receivedMessage.lastIndexOf(" ") + 1); //extract data from message, everything after last space
       char receiveBuffTime[9];
       char receiveBuffTs[7];
       (receivedData.substring(0, 7)).toCharArray(receiveBuffTime, 9);
-      int timeInteger = strtoul(receiveBuffTime, NULL, 16);
+      int timeInteger = strtoul(receiveBuffTime, NULL, 16); //convert hexadecimal time to integer
       (receivedData.substring(8, 13)).toCharArray(receiveBuffTs, 7);
       int timeIntegerTs = strtoul(receiveBuffTs, NULL, 16);
 
-      rtc.setHours(((timeInteger) / 3600000) % 24);
+      rtc.setHours(((timeInteger) / 3600000) % 24); // synchronise time
       rtc.setMinutes(((timeInteger) / 60000) % 60);
       rtc.setSeconds(((timeInteger) / 1000) % 60);
       rtc.setSubSeconds((timeInteger) % 1000);
 
-      tsMinutes = ((timeIntegerTs) / 60000) % 60;
+      tsMinutes = ((timeIntegerTs) / 60000) % 60; // set time slot
       tsSeconds = ((timeIntegerTs) / 1000) % 60;
       tsMilli = (timeInteger) % 1000;
 
-      criticalTemp = uint8_t((receivedData.substring(14, 17)).toInt());
-      offSeason = uint8_t((receivedData.substring(18, 19)).toInt());
+      criticalTemp = uint8_t((receivedData.substring(14, 17)).toInt()); //set critical temperture
+      offSeason = uint8_t((receivedData.substring(18, 19)).toInt());    //set offseason mode on or off based on received setting
 
       return 1;
     }
-    else if (receivedMessage == "mac_tx_ok")
+    else if (receivedMessage == "mac_tx_ok") // if no data is received, continue without changing settings
     {
       return 1;
     }
@@ -96,7 +94,7 @@ bool receive()
 bool request() //Function that is called when a response from the base station is desired.
 {
   UARTInterface.begin(4096);
-  UARTInterface.write("mac tx uncnf 2 FF"); //send transmission on port 2 so that the base station knows that it should send a time slot to the device.
+  UARTInterface.write("mac tx uncnf 2 FF"); //send transmission so the base station can return a time slot.
   return receive();
 }
 
@@ -108,7 +106,7 @@ bool join()
   digitalWrite(PA_9, LOW); //send break condition to rn2483 to trigger automatic baud rate detection
   delay(1);
   UARTInterface.begin(4096);
-  UARTInterface.write(0x55);
+  UARTInterface.write(0x55); //Send 0x55 as specified in the rn2483 datasheet.
 
   //setup lora module over uart
   char setDeveui[24];
@@ -116,39 +114,39 @@ bool join()
   char setNwks[49];
   char setApps[49];
 
-  strcpy(setDeveui, "mac set deveui ");
+  strcpy(setDeveui, "mac set deveui "); // set device ID
   strcat(setDeveui, deviceID);
   UARTInterface.write(setDeveui);
   UARTInterface.readString();
 
-  strcpy(setSF, "radio set sf ");
+  strcpy(setSF, "radio set sf "); // set spreading factor
   strcat(setSF, spreadingFactor);
   UARTInterface.write(setSF);
   UARTInterface.readString();
 
-  strcpy(setNwks, "mac set nwkskey ");
+  strcpy(setNwks, "mac set nwkskey "); // set network session key
   strcat(setNwks, nwkskey);
   UARTInterface.write(setNwks);
   UARTInterface.readString();
 
-  strcpy(setApps, "mac set appskey ");
+  strcpy(setApps, "mac set appskey "); // set application session key
   strcat(setApps, appskey);
   UARTInterface.write(setApps);
   UARTInterface.readString();
 
-  UARTInterface.write("mac join abp");
-  if (UARTInterface.readString() == "ok")
+  UARTInterface.write("mac join abp");    //send mac join command
+  if (UARTInterface.readString() == "ok") //if command transmission is succesful, go to sleep till next confirmation.
   {
     LowPower.deepSleep();
     if (UARTInterface.readString() == "accepted")
     {
-      return request();
+      return request(); // return to main loop if accepted to the network
     }
   }
   return 0;
 }
 
-int minFunc(int *temperatures)
+int minFunc(int *temperatures) //determine the lowest temperature measured at a certain time by the 5 sensors.
 {
   int min = temperatures[0];
   for (size_t i = 1; i < sensorCount; i++)
@@ -161,7 +159,7 @@ int minFunc(int *temperatures)
   return min;
 }
 
-String padLeft(String input, uint16_t desiredLength)
+String padLeft(String input, uint16_t desiredLength) // to make sure all inputs are the same length
 {
 
   while (input.length() < desiredLength)
@@ -173,7 +171,7 @@ String padLeft(String input, uint16_t desiredLength)
 
 void tempMeasurement()
 {
-  Wire.setClock(100000);
+  Wire.setClock(32768); // set i2c clock to 32768 kHz (max periperal clock)
   Wire.setSDA(PA_10);
   Wire.setSCL(PA_9);
   Wire.begin();
@@ -191,23 +189,23 @@ void tempMeasurement()
     int16_t MSB = Wire.read() * 256;
     int16_t LSB = Wire.read();
     int16_t value = MSB + LSB;
-    temperature[i] = value / 12.8; //due to roundoff error of +-0.2
+    temperature[i] = value / 12.8; //due to roundoff error of +-0.2. Interpret data as specified by datasheet
   }
   Wire.endTransmission(); // stop transmitting
   return;
 }
 
-bool transmitOffSeason(int energyLevel, int **temperatureArray)
+bool transmitOffSeason(int energyLevel, int **temperatureArray) // transmit temperature data of the measurements of 4 different times
 {
   char command[(offSeasonIterations * sensorCount * 4) + 18];
   char payload[(offSeasonIterations * sensorCount * 4) + 3];
-  String EL = String(int16_t(energyLevel), HEX);
-  String concatenated = padLeft(EL, 2);
+  String EL = String(int16_t(energyLevel), HEX); //convert current energy level to hexadecimal
+  String concatenated = padLeft(EL, 2);          //zero pad string to comply with length requirement
   for (size_t i = 0; i < offSeasonIterations; i++)
   {
     for (size_t j = 0; i < sensorCount; j++)
     {
-      concatenated = concatenated + padLeft(String(int16_t(temperatureArray[i][j]), HEX), 4);
+      concatenated = concatenated + padLeft(String(int16_t(temperatureArray[i][j]), HEX), 4); //add all temperatures measurements to string  in hexadecimal
     }
   }
 
@@ -216,12 +214,12 @@ bool transmitOffSeason(int energyLevel, int **temperatureArray)
   strcpy(command, "mac tx uncnf 3 ");
   strcat(command, payload);
 
-  UARTInterface.begin(57600);
+  UARTInterface.begin(4096);
   UARTInterface.write(command);
   return receive();
 }
 
-bool transmit(int energyLevel, int *temperature)
+bool transmit(int energyLevel, int *temperature) // Transmit the measured data
 {
   char command[sensorCount * 4 + 18];
   char payload[sensorCount * 4 + 3];
@@ -235,14 +233,14 @@ bool transmit(int energyLevel, int *temperature)
   concatenated.toCharArray(payload, sensorCount * 4 + 3);
 
   strcpy(command, "mac tx uncnf 1 ");
-  strcat(command, payload);
+  strcat(command, payload); // create the full command
 
-  UARTInterface.begin(57600);
+  UARTInterface.begin(4096);
   UARTInterface.write(command);
   return receive();
 }
 
-int closestPeriod()
+int closestPeriod() // find the maximum allowable time between measurements that detects fruit frost
 {
   int minTimeToNext = (minFunc(temperature) - criticalTemp) / maxTempSlope;
   int availablePeriods[5] = {30, 20, 15, 10, 5};
@@ -284,7 +282,7 @@ int nextTimeSeg(int currentTimeSegment, int measurementPeriod) // Calculate the 
   return availableTimeSegments[0]; //If the next time slot is after the passing of the hour, return the first segment in the hour.
 }
 
-void setup()
+void setup() // configure settings, join network
 {
   joined = 0;
   LowPower.enableWakeupFrom(&UARTInterface, NULL);
@@ -297,7 +295,7 @@ void setup()
   {
     if (checkEnergy() > threshold_Join)
     {
-      LowPower.deepSleep(int(random(1000, 5 * minute))); //sleep for a random time before trying to rejoin.
+      LowPower.deepSleep(int(random(1000, 5 * minute))); //sleep for a random time before trying to rejoin
       joined = join();                                   //join the LoRa network
     }
     else
@@ -307,7 +305,7 @@ void setup()
   }
 }
 
-bool powerSavingMode()
+bool powerSavingMode() // determine whether enough power is present in the system to perform 1 tranmission
 {
   int previousEnergy, currentEnergy;
   int sleepDuration;
@@ -343,14 +341,14 @@ bool powerSavingMode()
     int nextTimeSegment = nextTimeSeg(currentTimeSegment, 12);
     int nextHours = currentHours;
     int nextMinutes = nextTimeSegment * 5 + tsMinutes;
-    rtc.setAlarmTime(nextHours, nextMinutes, tsSeconds, tsMilli);
+    rtc.setAlarmTime(nextHours, nextMinutes, tsSeconds, tsMilli); // set alarm to next avaiale time slot
     LowPower.deepSleep();
     return 0;
   }
   return 1;
 }
 
-void normalMode()
+void normalMode() // adaptive temperature measurements based on current energy level and temperature
 {
   int measurementPeriodEL;
   int measurementPeriodTemp;
@@ -389,11 +387,11 @@ void normalMode()
   int nextHours = currentHours;
   int nextMinutes = nextTimeSegment * 5 + tsMinutes;
 
-  int transmitted = transmit(checkEnergy(), temperature);
+  int transmitted = transmit(checkEnergy(), temperature); //transmit energy level and temperature data
   if (transmitted == 0)
   {
-    syncCounter += 1;
-    if (syncCounter == syncThresholdNormal)
+    syncCounter += 1;                       //count number of transmissions without reply
+    if (syncCounter == syncThresholdNormal) //rejoin network if too many transmissions are unanswered
     {
       int synced = 0;
       while (synced == 0)
@@ -420,7 +418,7 @@ void normalMode()
   }
   else
   {
-    syncCounter = 0;
+    syncCounter = 0; //reset counter if reply is received
   }
 
   rtc.setAlarmTime(nextHours, nextMinutes, tsSeconds, tsMilli);
@@ -428,7 +426,7 @@ void normalMode()
   return;
 }
 
-void offSeasonMode()
+void offSeasonMode() //collect 4 measurements and transmit those in one transmission
 {
   int *tempArray[5];
 
@@ -438,7 +436,7 @@ void offSeasonMode()
     tempMeasurement();
     tempArray[i] = temperature;
   }
-  // Attempt to retransmit for a maximum of 3 times.
+  //Transmit data. If more than 5 transmissions have been done without synchronisation message from base station, rejoin the network.
   int transmitted = transmitOffSeason(checkEnergy(), tempArray);
   if (transmitted == 0)
   {
@@ -477,7 +475,7 @@ void offSeasonMode()
   return;
 }
 
-void loop()
+void loop() //loop though the various modes
 {
   if (powerSavingMode())
   {
